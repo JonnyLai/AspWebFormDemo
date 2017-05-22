4146453ca02f7c28b4458776fb3b2fb31317a185
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-22 19:10:45 +0800

    Create New

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Hello.World.QiyaChiChi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HelloWorld hello = new HelloWorld();
+            Hello.World.QiyaChiChi.HelloWorld helloWorld = new Hello.World.QiyaChiChi.HelloWorld();
+        }
+    }
+}
+
+namespace Hello.World.QiyaChiChi
+{
+    public class HelloWorld
+    {
+        public string HelloWorldPerportyA { set; get; }
+
+        public void HelloWorldFunctionA()
+        {
+
+        }
+    }
+}
